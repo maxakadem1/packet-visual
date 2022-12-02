36a058087c3411e432b045b1a9f5edf2f17a5389
ExoWrathe <ethanducharm@gmail.com>
2022-12-02 00:00:35 -0600

    Created the python server
    Create the python server with flask restful api.

---
+++ server.py
@@ -0,0 +1,43 @@
+from flask import Flask, jsonify, request
+from flask_restful import Resource, Api
+
+# creating the flask app
+app = Flask(__name__)
+# creating an API object
+api = Api(app)
+
+#Class for different resources/Paths 
+#Each class will be possess their own get/post/put/del methods which correspond 
+#to the REST API methods being called by the react portion of the app
+class userData(Resource):
+
+    def get(self):
+        data = {"Data:":"Here"}
+
+        return jsonify(data)
+    
+    def post(self):
+        #Add the downloading of data here
+        
+        return jsonify()
+
+class sampleData(Resource):
+
+    def get(self):
+        sampledata = "Blank temp"
+        return jsonify(sampledata)
+
+class homePage(Resource):
+
+    def get(self):
+        homePage = "Temp home page"
+        return homePage
+
+
+api.add_resource(homePage, '/')
+api.add_resource(userData, '/userData')
+api.add_resource(sampleData,'/sampleData')
+
+if __name__ == '__main__':
+
+    app.run(debug=True)

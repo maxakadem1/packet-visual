0e08908132d9e454ae130aec498a6c2a7bff0a92
Maxim Abdulkhalikov <63835313+maxakadem1@users.noreply.github.com>
2022-12-09 14:37:50 -0600

    Added a basic JSON reader in app.js

---
+++ .history/src/locations_20221209135858.js
@@ -0,0 +1,15 @@
+export const usa = {
+  longitude: -98.12078906038815,
+  latitude: 39.07050154936433,
+  zoom: 3.847,
+  pitch: 41.8,
+  bearing: 0.7,
+};
+
+export const winnipeg = {
+  longitude: -97.138451,
+  latitude: 49.895077,
+  zoom: 11,
+  pitch: 41.8,
+  bearing: 0.7,
+};
--- src/locations.js
+++ src/locations.js
@@ -6,34 +6,10 @@ export const usa = {
   bearing: 0.7,
 };
 
-export const boston = {
-  longitude: -71.091908,
-  latitude: 42.362419,
-  zoom: 12,
-  pitch: 47.5,
-  bearing: -1.45,
-};
-
-export const coolidge = {
-  longitude: -71.121316,
-  latitude: 42.342492,
-  zoom: 12,
-  pitch: 0,
-  bearing: 0,
-};
-
-export const ma = {
-  longitude: -71.84284514317822,
-  latitude: 42.16213680615227,
-  zoom: 7.5,
-  pitch: 0,
-  bearing: 0,
-};
-
-export const nyc = {
-  longitude: -73.9544312807859,
-  latitude: 40.737897096399855,
+export const winnipeg = {
+  longitude: -97.138451,
+  latitude: 49.895077,
   zoom: 11,
-  pitch: 42.9,
-  bearing: 58.7,
+  pitch: 41.8,
+  bearing: 0.7,
 };
